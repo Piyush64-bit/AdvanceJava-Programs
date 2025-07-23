bef5622e5ff430d6184048e0cc9f1ea4ced4ff6b
Piyush00Soni <Piiyush.sonii@outlook.com>
2025-07-23 12:23:21 +0530

    Today - Enum

---
+++ 02Enumeration/EnumOne.java
@@ -0,0 +1,19 @@
+
+enum color {
+    RED("red"), GREEN("green"), BLUE("blue");
+
+    private String value;
+
+    color(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+
+    }
+}
+
+// 1> With the help of enumeration in java, i can declare any constant whatsoever in java and use it anywhere in my code.
+// 2> Here, i have used constants RED, GREEN and BLUE along with that thier values, so when they are called, thier values present inside them is also shown.
+// 3> They can be allocated in diffrent files, without creating and object
